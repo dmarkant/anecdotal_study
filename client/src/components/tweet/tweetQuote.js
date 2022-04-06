3b364f625ebcc635f1385794c6bb60640ac73f8f
alireza116 <alireza116@gmail.com>
2022-04-05 22:03:19 -0500

    fixed material dependency

--- client/src/components/tweet/tweetQuote.js
+++ client/src/components/tweet/tweetQuote.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@mui/styles";
 import { Typography } from "@mui/material";
 
 var parser = new DOMParser();
